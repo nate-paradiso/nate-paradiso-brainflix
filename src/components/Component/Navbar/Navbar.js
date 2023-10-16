e6a14f17212815e803fa913bbff11eb637c4161b
Nate Paradiso <ipfcolo05@gmail.com>
2023-10-16 16:59:19 -0700

    nav bar complete

--- src/components/Component/Navbar/Navbar.js
+++ src/components/Component/Navbar/Navbar.js
@@ -7,22 +7,23 @@ function NavBar () {
     return(
         <header className="header">
             <div className="header__logo">
-                <img src={logo}
-                alt="logo">
-                </img>
+                <img src={logo} alt="logo"></img>
             </div>
             <div className="header__search-container">
                 <form>
                     <input type="text" placeholder="Search" name="search"></input>
                 </form>
                 <div class="header__img-container">
-                        <div class="header__img-container--image">
-                        </div>
+                    <div class="header__img-container--image">
+                    </div>
                 </div>
             </div>
             <button className="header__button">UPLOAD</button>
+            <div class="header__img-container-tablet">
+                <div class="header__img-container--image-tablet">
+                </div>
+            </div>
         </header>
-
     );
 }
 export default NavBar
