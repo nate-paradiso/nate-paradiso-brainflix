f8c9027d050f5e467cbfd6a067124709e93027f3
Nate Paradiso <ipfcolo05@gmail.com>
2023-10-16 17:06:15 -0700

    starting hero branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.scss';
 import NavBar from './components/Component/Navbar/Navbar';
+import Hero from './components/Component/Hero/Hero';
 
 function App() {
   return (
     <div>
     <NavBar/>
+    <Hero/>
     
     </div>
   );
---
+++ src/components/Component/Hero/Hero.js
@@ -0,0 +1,10 @@
+import "./Hero.scss";
+
+
+function Hero () {
+
+    return(
+        <p>hello</p>
+    )
+}
+export default Hero;
