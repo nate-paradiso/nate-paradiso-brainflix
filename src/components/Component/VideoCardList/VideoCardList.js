442a7aa7e27a3007cbf72f315a88b353fce207ad
Nate Paradiso <ipfcolo05@gmail.com>
2023-10-17 10:47:27 -0700

    started video card list

---
+++ src/components/Component/VideoCardList/VideoCardList.js
@@ -0,0 +1,14 @@
+import "./VideoCardList.scss";
+import videoData from "../../../data/videos.json"
+import videoDataDetails from "../../../data/video-details.json"
+
+export function VideoCardList () {
+    console.log(videoData)
+    console.log(videoDataDetails)
+
+    return(
+        <>
+            <video>placeholder</video>
+        </>
+    )
+}
