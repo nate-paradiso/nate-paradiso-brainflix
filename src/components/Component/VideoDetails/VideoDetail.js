c6f1197b57a4aa8368caec3e70bc67c765beff74
Nate Paradiso <ipfcolo05@gmail.com>
2023-10-19 15:26:39 -0700

    details is working

--- src/components/Component/VideoDetails/VideoDetail.js
+++ src/components/Component/VideoDetails/VideoDetail.js
@@ -1,32 +1,59 @@
 import "./VideoDetails.scss";
 import videoDetails from "../../../data/video-details.json"
+
+
 export function CurrentVideoDetails ( {selectedVideoId}) {
 console.log(videoDetails)
 
+const formatTime = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toLocaleDateString();
+}
     return(
         <>
-        <h1 className="Title">{videoDetails
+        <h1 className="title">{videoDetails
         .find(videoImage => videoImage.id === selectedVideoId)
         .title}</h1>
-        <div className="Data">
-            <div className="Data__container">
-                <p>By {videoDetails
-                .find(videoImage => videoImage.id === selectedVideoId)
-                .channel}
+        <div className="data">
+            <div className="data__container">
+                <p>
+                    By {videoDetails
+                    .find(videoImage => videoImage.id === selectedVideoId)
+                    .channel}
                 </p>
-                <img src="" alt="" />
-                <p>{videoDetails
-                .find(videoImage => videoImage.id === selectedVideoId)
-                .likes}
-                </p>
+                <img src="../../../" alt="views image" />
+                <p>
+                    {formatTime(videoDetails
+                    .find(videoImage => videoImage.id === selectedVideoId)
+                    .timestamp)}
+                </p>            
             </div>
-            <div className="Data__container">
-                <p></p>
+            <div className="data__container">
+                <p>
+                    {videoDetails
+                    .find(videoImage => videoImage.id === selectedVideoId)
+                    .views}
+                </p>
                 <img src="" alt="" />
-                <p></p>
+                <p>
+                    {videoDetails
+                    .find(videoImage => videoImage.id === selectedVideoId)
+                    .likes}
+                </p>
             </div>
         </div>
-        <p></p>
+        <div>
+            <p>
+                {videoDetails
+                .find(videoImage => videoImage.id === selectedVideoId)
+                .description}
+            </p>
+            <p>
+                {videoDetails
+                .find(videoImage => videoImage.id === selectedVideoId)
+                .comments.length} Comments
+            </p>
+        </div>
         </>
     )
 }
