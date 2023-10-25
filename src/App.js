1ec834efb9c2797291d436ad1a8fee1a569ab78c
Nate Paradiso <ipfcolo05@gmail.com>
2023-10-25 14:42:56 -0700

    refactored alot of code and removed home page

--- src/App.js
+++ src/App.js
@@ -4,46 +4,21 @@ import './App.scss';
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Upload } from './pages/Upload/Upload';
-import { HomePage } from './pages/HomePage/HomePage';
 import VideoPlayerPage from './pages/VideoPlayerPage/VideoPlayerPage';
 import axios from 'axios';
 
 function App() {
 
-  const formatTime = (timestamp) => {
-    const date = new Date(timestamp);
-    return date.toLocaleDateString();
-  }
-  const [ videoData, setVideoData] = useState ([])
-  // const apiEndpoint = "https://project-2-api.herokuapp.com/videos?api_key=b83df1dc-dac4-4015-afac-d72c99d85694"
   
-  useEffect(() => {
-    const fetchVideo = async () => {
-      const response = await axios.get("https://project-2-api.herokuapp.com/videos?api_key=b83df1dc-dac4-4015-afac-d72c99d85694");
-      console.log(response);
-      setVideoData(response.data);
-    }
-    fetchVideo();
-  }, [])
-
-  
-  // const handleVideoData = (id) => {
-  //   setVideoData(id)
-  // }
   return (
     <BrowserRouter>
       <NavBar/>
       <Routes>
-        <Route path="/" element={<HomePage 
-        formatTime={formatTime}
-        videoData={videoData}
-        setVideoData={setVideoData} 
+        <Route path="/" element={<VideoPlayerPage  
         />} />
         <Route path="/upload" element={<Upload />} /> 
-        <Route path="/video/videoId" element={<VideoPlayerPage 
-         formatTime={formatTime}
-         videoData={videoData}
-        setVideoData={setVideoData}  />} />
+        <Route path="/video/:videoId" element={<VideoPlayerPage 
+        />} />
        </Routes>
        </BrowserRouter>
   )
--- src/pages/VideoPlayerPage/VideoPlayerPage.js
+++ src/pages/VideoPlayerPage/VideoPlayerPage.js
@@ -3,32 +3,67 @@ import { Form } from '../../components/Component/Form/Form';
 import { CommentCardList } from '../../components/Component/CommentCardList/CommentCardList';
 import { VideoCardList } from '../../components/Component/VideoCardList/VideoCardList';
 import { VideoDetails } from '../../components/Component/VideoDetails/VideoDetail';
+import { useParams } from 'react-router-dom'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-const VideoPlayerPage = (formatTime, selectedVideoId, handleSelectVideo) => {
+const VideoPlayerPage = () => {
+
+  // const formatTime = (timestamp) => {
+  //   const date = new Date(timestamp);
+  //   return date.toLocaleDateString();
+  // }
+
+  const { videoId } = useParams();
+  // const [ videoDataId, setVideoDataId] = useState ([])
+  const [ videoData, setVideoData] = useState ([])
+
+
+  
+  useEffect(() => {
+    const fetchVideoId = async () => {
+      const response = await axios.get(`https://project-2-api.herokuapp.com/videos/${videoId ? videoId : "84e96018-4022-434e-80bf-000ce4cd12b8" }?api_key=b83df1dc-dac4-4015-afac-d72c99d85694`);
+      console.log(response);
+      // setVideoData(response.data);
+    }
+    fetchVideoId();
+  }, [])
+
+  useEffect(() => {
+    const fetchVideo = async () => {
+      const response = await axios.get("https://project-2-api.herokuapp.com/videos?api_key=b83df1dc-dac4-4015-afac-d72c99d85694");
+      console.log(response);
+      setVideoData(response.data);
+    }
+    fetchVideo();
+  }, [])
+
+  if (videoData) console.log(videoData)
   return (
     <>
+    {videoData ?  <>
     <header >
-     <CurrentVideo
-     selectedVideoId={selectedVideoId}
-     />
+     {/* <CurrentVideo
+    setVideoDataId={setVideoDataId} 
+     /> */}
    </header>
    <main className="main">
-     <section className="main__comments">
+     {/* <section className="main__comments">
        <VideoDetails
        formatTime={formatTime}
-       selectedVideoId={selectedVideoId}
+       videoData={videoData}
        />
        <Form/>    
        <CommentCardList
        formatTime={formatTime}
-       selectedVideoId={selectedVideoId}
+       videoData={videoData}
        />
-     </section>  
-     <VideoCardList
-     selectedVideoId={selectedVideoId}
-     handleSelectVideo={handleSelectVideo}
-     />    
-   </main>
+     </section>   */}
+    <VideoCardList
+      videoData={videoData}
+      setVideoData={setVideoData}  
+      /> 
+   </main> </> : <p>"loading..."</p> }
    </>
   )
 }
